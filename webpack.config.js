fe3e14b04117ce6888252479b64fbf40554b64ec
parkhojeong <parkhj062@gmail.com>
2024-10-07 23:30:42 +0900

    # 1-webpack/2-loader solve

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,26 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            }
+          }
+        ]
+
+
+      }
+    ]
   }
 }
